bf0cd731012f812b69554a8a50f811b156e313d0
avneet1604 <avneet.devgan@gmail.com>
2021-07-20 12:33:54 +0530

    created Home screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,76 @@
 import React, {Component} from 'react';
-import { Text, View } from 'react-native';
+import {View,
+    Text,
+    StyleSheet,
+    SafeAreaView,
+    TouchableOpacity,
+    Platform,
+    StatusBar,
+    ImageBackground,
+    Image } from 'react-native';
 
 export default class HomeScreen extends Component{
     render(){
         return(
-            <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
-                <Text>Home Screen</Text>
+            <View style={styles.container}>
+                <SafeAreaView style={styles.droidSafeArea} />
+                <ImageBackground source={require('../assets/bg2.jpg')} style={styles.backgroundImage}>
+                <View style={styles.titleBar}>
+                    <Text style={styles.titleText}> FUNLEARN </Text>
+                </View>
+
+                <TouchableOpacity style={styles.routeCard}
+                onPress={()=>this.props.navigation.navigate("Main")} 
+                 >
+                  <Text style={styles.clickHere}> Click here to Learn--- </Text>
+                  <Image source={require("../assets/play.png")} style={styles.iconImage}></Image>
+                </TouchableOpacity>
+                </ImageBackground>
             </View>
         )
     }
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    droidSafeArea: {
+        marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+    },
+    backgroundImage: {
+        flex: 1,
+        resizeMode: 'cover',
+    },
+    titleBar: {
+        flex: 0.15,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    titleText: {
+        fontSize: 50,
+        fontWeight: "bold",
+        color: "black",
+        marginTop:230
+    },
+    routeCard: {
+        flex: 0.25,
+        marginLeft: 50,
+        marginRight: 50,
+        marginTop: 250,
+        borderRadius: 30,
+    },
+    clickHere: {
+        paddingLeft: 30,
+        color: "green",
+        fontSize: 20
+    },
+    iconImage: {
+        position: "absolute",
+        height: 100,
+        width: 100,
+        resizeMode: "contain",
+        right: 5,
+        top: 30,
+        marginRight:70
+    },
+})
